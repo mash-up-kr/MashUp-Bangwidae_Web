094e8845350e24acf80a17eaaa29a0d7377ea319
Sangwoo Park <sanoopark@gmail.com>
2022-07-09 21:19:28 +0900

    feat: reset css 적용 (#8)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css';
+import '@/styles/globals.css';
+import '@/styles/reset.css';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
