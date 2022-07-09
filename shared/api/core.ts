d8be5e3288fdb4741c86bd4141569bd096fd0e9b
김동현G <donghyeon@toss.im>
2022-07-09 17:11:59 +0900

    feat: axios 추상화 작업 구현

---
+++ shared/api/core.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosInstance, AxiosRequestConfig, Method } from 'axios';
+import { HTTP_METHODS } from '@/consts';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: '',
+  timeout: 10000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+const createApiMethod =
+  (_axiosInstance: AxiosInstance, methodType: Method) =>
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  (config: AxiosRequestConfig): Promise<any> => {
+    _axiosInstance.interceptors.response.use((response) => {
+      if (!response.data) return response;
+      return response.data;
+    });
+
+    return _axiosInstance({
+      ...config,
+      method: methodType,
+    });
+  };
+
+export default {
+  get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
+  post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
+  patch: createApiMethod(axiosInstance, HTTP_METHODS.PATCH),
+  put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
+  delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
+};
