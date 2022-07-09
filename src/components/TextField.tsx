63ffe446d2909d4b3ec616c8ed5304c1ec323935
yeomhyeseon <75597374+yeomhyeseon@users.noreply.github.com>
2022-07-10 04:39:13 +0900

    feat: setting > MyProfile 작업 (#23)
    * feat: TextField 컴포넌트 제작
    
    * feat: setting my page

---
+++ src/components/TextField.tsx
@@ -0,0 +1,96 @@
+/* eslint-disable react/require-default-props */
+import { ChangeEvent } from 'react';
+import styled from 'styled-components';
+import { typography } from '@/styles';
+
+interface TextFieldProps {
+  value: string;
+  label?: string;
+  maxLength?: number;
+  placeholder?: string;
+  hintText?: string;
+  className?: string;
+  onChange: (value: string) => void;
+}
+
+function TextField({
+  value,
+  label,
+  maxLength,
+  placeholder,
+  hintText,
+  className,
+  onChange,
+}: TextFieldProps) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  return (
+    <Element className={className}>
+      {label && <Label>{label}</Label>}
+      <Input value={value} onChange={handleChange} placeholder={placeholder} isError={!!hintText} />
+      <BottomSection>
+        <HintText>{hintText}</HintText>
+        {maxLength && (
+          <MaxLength>
+            {value.length}
+            <span>/{maxLength}</span>
+          </MaxLength>
+        )}
+      </BottomSection>
+    </Element>
+  );
+}
+
+export default TextField;
+
+const Element = styled.div`
+  position: relative;
+`;
+
+const Label = styled.div`
+  color: ${({ theme }) => theme.color.gray.Gray500};
+  ${typography.Body_Regular_14}
+`;
+
+const Input = styled.input<{ isError?: boolean }>`
+  width: 100%;
+  padding: 16px 0 13px;
+  color: ${({ theme }) => theme.color.basic.White};
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid
+    ${({ theme, isError }) => (isError ? theme.color.error.Red500 : theme.color.gray.Gray800)};
+  box-shadow: none;
+  caret-color: ${({ theme }) => theme.color.primary.Lime300};
+  ${typography.Giant2_Bold_20}
+  &:focus {
+    border-bottom: 1px solid
+      ${({ theme, isError }) => (isError ? theme.color.error.Red500 : theme.color.primary.Lime300)};
+    outline: none;
+  }
+  ::placeholder {
+    color: ${({ theme }) => theme.color.gray.Gray700};
+  }
+`;
+
+const BottomSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HintText = styled.div`
+  margin-top: 7px;
+  color: ${({ theme }) => theme.color.error.Red500};
+  ${typography.Caption1_Regular_13}
+`;
+
+const MaxLength = styled.div`
+  margin-top: 7px;
+  ${typography.Caption1_Regular_13}
+  color: ${({ theme }) => theme.color.gray.Gray500};
+  span {
+    color: ${({ theme }) => theme.color.gray.Gray700};
+  }
+`;
--- src/pages/QuestionDetail/index.tsx
+++ src/pages/QuestionDetail/index.tsx
@@ -1,5 +1,25 @@
+import { useState } from 'react';
+import TextField from '@/src/components/TextField';
+
 function QuestionDetail() {
-  return <div>질문 상세페이지입니다.</div>;
+  const [value, setValue] = useState('');
+  const handleChange = (v: string) => {
+    setValue(v);
+  };
+
+  return (
+    <>
+      <div>질문 상세페이지입니다.</div>
+      <TextField
+        value={value}
+        onChange={handleChange}
+        label="라벨"
+        placeholder="플레이스홀더"
+        maxLength={10}
+        hintText={value.length > 10 ? '힌트텍스트입니다.' : undefined}
+      />
+    </>
+  );
 }
 
 export default QuestionDetail;
