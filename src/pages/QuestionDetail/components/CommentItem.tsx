00e84f379a7a8112e5931748f428b9014971d622
sanoopark <sanoopark@gmail.com>
2022-07-26 00:21:14 +0900

    feat: question detail styling

---
+++ src/pages/QuestionDetail/components/CommentItem.tsx
@@ -0,0 +1,135 @@
+import styled from 'styled-components';
+import { Icon } from '@/src/components';
+import { typography } from '@/styles';
+
+function QuestionDetail() {
+  return (
+    <Layout>
+      <FlexRow gap={8}>
+        <ProfileImage src="https://picsum.photos/200" />
+        <FlexBetween>
+          <FlexColumn gap={6}>
+            <FlexRow gap={8}>
+              <Nickname>도리를 찾아서</Nickname>
+              <LevelTag>Lv.1</LevelTag>
+            </FlexRow>
+          </FlexColumn>
+        </FlexBetween>
+        <FlexRow gap={8}>
+          <LocatedAt>강남구</LocatedAt>
+          <CreatedAt>1분 전</CreatedAt>
+          <IconPosition>
+            <Icon name="more" color="#767676" size={24} onClick={() => {}} />
+          </IconPosition>
+        </FlexRow>
+      </FlexRow>
+      <CommentContent>니모의 절친 도리를 모른다고??</CommentContent>
+      <FlexBetween>
+        <FlexRow gap={8}>
+          <Icon color="#767676" size={24} onClick={() => {}}>
+            답글 달기
+          </Icon>
+          <VerticalDivider />
+          <Icon color="#DBF87A" size={24} onClick={() => {}}>
+            좋아요
+          </Icon>
+        </FlexRow>
+        <Icon name="heart" color="#DBF87A" size={24} textPosition="left" onClick={() => {}}>
+          <IconText>1</IconText>
+        </Icon>
+      </FlexBetween>
+    </Layout>
+  );
+}
+
+export default QuestionDetail;
+
+const Layout = styled.div`
+  padding: 17px 0;
+  border-bottom: 1px solid ${({ theme }) => theme.color.gray.Gray800};
+`;
+
+const FlexRow = styled.div<{
+  gap: number;
+}>`
+  display: flex;
+  align-items: center;
+  column-gap: ${({ gap }) => `${gap}px`};
+`;
+
+const ProfileImage = styled.img`
+  width: 32px;
+  height: 32px;
+  border: 1px solid #3a3a3a;
+  border-radius: 50%;
+`;
+
+const FlexBetween = styled.div`
+  display: flex;
+  flex-grow: 1;
+  justify-content: space-between;
+`;
+
+const FlexColumn = styled.div<{
+  gap: number;
+}>`
+  display: flex;
+  flex-direction: column;
+  row-gap: ${({ gap }) => `${gap}px`};
+`;
+
+const Nickname = styled.div`
+  ${typography.Body_Medium_14}
+`;
+
+const LevelTag = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 42px;
+  height: 18px;
+  color: ${({ theme }) => theme.color.gray.Gray800};
+  font-weight: 700;
+  font-size: 12px;
+  background-color: ${({ theme }) => theme.color.primary.Lime300};
+  border-radius: 40px;
+`;
+
+const LocatedAt = styled.div`
+  color: ${({ theme }) => theme.color.gray.Gray600};
+  font-weight: 400;
+  ${typography.Caption2_Regular_12}
+
+  ::after {
+    height: 10px;
+    margin-left: 10px;
+    border-right: 1px solid ${({ theme }) => theme.color.gray.Gray800};
+    content: '';
+  }
+`;
+
+const CreatedAt = styled.div`
+  color: ${({ theme }) => theme.color.gray.Gray600};
+  font-weight: 400;
+  ${typography.Caption2_Regular_12}
+`;
+
+const IconPosition = styled.div`
+  padding-top: 4px;
+`;
+
+const IconText = styled.span`
+  color: white;
+`;
+
+const CommentContent = styled.div`
+  margin: 10px 0;
+  padding: 10px 0;
+  color: white;
+  font-size: 14px;
+`;
+
+const VerticalDivider = styled.div`
+  height: 10px;
+  border-right: 1px solid ${({ theme }) => theme.color.gray.Gray800};
+`;
--- src/pages/QuestionDetail/index.tsx
+++ src/pages/QuestionDetail/index.tsx
@@ -1,25 +1,263 @@
-import { useState } from 'react';
-import TextField from '@/src/components/TextField';
+import styled from 'styled-components';
+import { LargeLineButton, Icon } from '@/src/components';
+import { typography } from '@/styles';
+import { CommentItem } from './components';
 
 function QuestionDetail() {
-  const [value, setValue] = useState('');
-  const handleChange = (v: string) => {
-    setValue(v);
-  };
-
   return (
-    <>
-      <div>질문 상세페이지입니다.</div>
-      <TextField
-        value={value}
-        onChange={handleChange}
-        label="라벨"
-        placeholder="플레이스홀더"
-        maxLength={10}
-        hintText={value.length > 10 ? '힌트텍스트입니다.' : undefined}
-      />
-    </>
+    <Layout>
+      {/* Top Section */}
+      <TopSection>
+        {/* Header */}
+        <FlexRow gap={8}>
+          <ProfileImage src="https://picsum.photos/200" />
+          <FlexBetween>
+            <FlexColumn gap={6}>
+              <FlexRow gap={8}>
+                <Nickname>도리를 찾아서</Nickname>
+                <LevelTag>Lv.1</LevelTag>
+              </FlexRow>
+              <FlexRow gap={6}>
+                <InterestTag>디즈니</InterestTag>
+                <InterestTag>영화</InterestTag>
+                <InterestTag>애니메이션</InterestTag>
+              </FlexRow>
+            </FlexColumn>
+            <LargeLineButton buttonType="default" onClick={() => {}}>
+              질문하기
+            </LargeLineButton>
+          </FlexBetween>
+        </FlexRow>
+        <Divider />
+        {/* Content */}
+        <Content>
+          도리를 찾아서가 뭐에요? 뭔지 찾으려 하는뎅 대체 #도리 가 무엇인지 누가 좀 알려줄 분~?
+        </Content>
+        <FlexRow gap={8}>
+          <LocatedAt>강남구</LocatedAt>
+          <CreatedAt>1분 전</CreatedAt>
+        </FlexRow>
+        {/* Menu Group */}
+        <MenuGroupPosition>
+          <MenuGroup>
+            <LeftIcon name="hand" color="#767676" size={20} onClick={() => {}}>
+              궁금해요
+            </LeftIcon>
+            <CenterIcon name="chat" color="#767676" size={20} onClick={() => {}}>
+              댓글
+            </CenterIcon>
+            <RightIcon name="share" color="#767676" size={20} onClick={() => {}}>
+              공유
+            </RightIcon>
+          </MenuGroup>
+        </MenuGroupPosition>
+      </TopSection>
+      {/* Bottom Section */}
+      <BottomSection>
+        <CommentItem />
+        <CommentInputWrapper>
+          <CommentInput type="text" placeholder="댓글을 남겨주세요." />
+          <CommentSubmitButton>
+            <LargeLineButton buttonType="primary" onClick={() => {}}>
+              등록
+            </LargeLineButton>
+          </CommentSubmitButton>
+        </CommentInputWrapper>
+      </BottomSection>
+    </Layout>
   );
 }
+
+const Layout = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  padding-top: 24px;
+`;
+
+/* Top Section */
+
+const TopSection = styled.div`
+  padding: 0 30px;
+`;
+
+const FlexRow = styled.div<{
+  gap: number;
+}>`
+  display: flex;
+  align-items: center;
+  column-gap: ${({ gap }) => `${gap}px`};
+`;
+
+const ProfileImage = styled.img`
+  width: 52px;
+  height: 52px;
+  border: 1px solid #3a3a3a;
+  border-radius: 50%;
+`;
+
+const FlexBetween = styled.div`
+  display: flex;
+  flex-grow: 1;
+  justify-content: space-between;
+`;
+
+const FlexColumn = styled.div<{
+  gap: number;
+}>`
+  display: flex;
+  flex-direction: column;
+  row-gap: ${({ gap }) => `${gap}px`};
+`;
+
+const Nickname = styled.div`
+  ${typography.Body_Medium_14}
+`;
+
+const LevelTag = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 42px;
+  height: 18px;
+  color: ${({ theme }) => theme.color.gray.Gray800};
+  font-weight: 700;
+  font-size: 12px;
+  background-color: ${({ theme }) => theme.color.primary.Lime300};
+  border-radius: 40px;
+`;
+
+const InterestTag = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 20px;
+  padding: 1px 6px;
+  color: ${({ theme }) => theme.color.gray.Gray400};
+  font-weight: 700;
+  font-size: 12px;
+  text-align: center;
+  background-color: ${({ theme }) => theme.color.gray.Gray800};
+  border-radius: 4px;
+`;
+
+const Divider = styled.hr`
+  margin: 16px 0;
+  color: ${({ theme }) => theme.color.gray.Gray800};
+  border: ${({ theme }) => `1px solid ${theme.color.gray.Gray800}`};
+`;
+
+const Content = styled.div`
+  margin: 16px 0;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 27.2px;
+  word-break: keep-all;
+`;
+
+const LocatedAt = styled.div`
+  color: ${({ theme }) => theme.color.gray.Gray600};
+  font-weight: 400;
+  ${typography.Caption2_Regular_12}
+
+  ::after {
+    height: 10px;
+    margin-left: 10px;
+    border-right: 1px solid ${({ theme }) => theme.color.gray.Gray700};
+    content: '';
+  }
+`;
+
+const CreatedAt = styled.div`
+  color: ${({ theme }) => theme.color.gray.Gray600};
+  font-weight: 400;
+  ${typography.Caption2_Regular_12}
+`;
+
+/* Menu Group */
+
+const MenuGroupPosition = styled.div`
+  position: absolute;
+  left: 0;
+  width: 100%;
+  height: 64px;
+`;
+
+const MenuGroup = styled.div`
+  position: absolute;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin-top: 27px;
+  margin-bottom: 20px;
+`;
+
+const LeftIcon = styled(Icon)`
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  height: 24px;
+`;
+
+const CenterIcon = styled(Icon)`
+  flex-grow: 1;
+  justify-content: center;
+  height: 24px;
+  border-right: ${({ theme }) => `1px solid ${theme.color.gray.Gray800}`};
+  border-left: ${({ theme }) => `1px solid ${theme.color.gray.Gray800}`};
+`;
+
+const RightIcon = styled(Icon)`
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  height: 24px;
+`;
+
+/* Bottom Section */
+
+const BottomSection = styled.div`
+  height: 100%;
+  margin-top: 64px;
+  padding: 0 30px;
+  background-color: black;
+  border-top: 1px solid ${({ theme }) => theme.color.gray.Gray800};
+`;
+
+const CommentInputWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  background-color: #121212;
+`;
+
+const CommentInput = styled.input`
+  width: calc(100% - 120px);
+  height: 58px;
+  margin-left: 30px;
+  color: white;
+  background-color: transparent;
+  border: none;
+  ${typography.Title2_Regular_16}
+  caret-color: ${({ theme }) => theme.color.primary.Lime300};
+  &:focus {
+    outline: none;
+  }
+  ::placeholder {
+    color: ${({ theme }) => theme.color.gray.Gray700};
+  }
+`;
+
+const CommentSubmitButton = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin-right: 30px;
+  transform: translateY(50%);
+`;
 
 export default QuestionDetail;
