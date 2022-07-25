2985a7d7b4207dd4b96a472a211deaa5309cda27
sanoopark <sanoopark@gmail.com>
2022-07-26 00:20:39 +0900

    feat: change icon component

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -6,15 +6,18 @@ import Chat from '../../public/icons/chat.svg';
 import Hand from '../../public/icons/hand.svg';
 import Heart from '../../public/icons/heart.svg';
 import Share from '../../public/icons/share.svg';
+import More from '../../public/icons/more.svg';
 
-type Name = 'chat' | 'hand' | 'heart' | 'share';
+type Name = 'chat' | 'hand' | 'heart' | 'share' | 'more';
+type TextPosition = 'left' | 'right';
 
 export interface IconProps {
-  name: Name;
+  name?: Name;
   color: string;
   size: number;
-  children: ReactNode;
+  children?: ReactNode;
   className?: string;
+  textPosition?: TextPosition;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
 
@@ -23,14 +26,30 @@ const icons = {
   hand: <Hand />,
   heart: <Heart />,
   share: <Share />,
+  more: <More />,
 };
 
-function Icon({ name, color, size, children, className, onClick }: IconProps) {
+function Icon({ name, color, size, children, className, textPosition, onClick }: IconProps) {
+  if (textPosition === 'left') {
+    return (
+      <Button name={name} color={color} onClick={onClick} className={className}>
+        <TextContent>{children}</TextContent>
+        {name && (
+          <SvgWrap color={color} size={size} includeText={!!children}>
+            {icons[name]}
+          </SvgWrap>
+        )}
+      </Button>
+    );
+  }
+
   return (
     <Button name={name} color={color} onClick={onClick} className={className}>
-      <SvgWrap color={color} size={size}>
-        {icons[name]}
-      </SvgWrap>
+      {name && (
+        <SvgWrap color={color} size={size} includeText={!!children}>
+          {icons[name]}
+        </SvgWrap>
+      )}
       <TextContent>{children}</TextContent>
     </Button>
   );
@@ -41,8 +60,9 @@ export default Icon;
 const SvgWrap = styled.div<{
   size: number;
   color: string;
+  includeText: boolean;
 }>`
-  padding-right: 4px;
+  padding: ${({ includeText }) => includeText && '0 4px'};
 
   svg {
     width: ${({ size }) => size};
@@ -69,6 +89,7 @@ const Button = styled.button<{
 `;
 
 const TextContent = styled.span`
+  padding-bottom: 4px;
   ${typography.Caption2_Bold_12}
   font-weight: 400;
 `;
