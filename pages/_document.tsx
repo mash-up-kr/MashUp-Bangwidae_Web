0dfe0d87346defef290d9ba3704387fba76ad8c0
김동현G <donghyeon@toss.im>
2022-08-07 22:21:39 +0900

    feat: modal-root element 추가

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <div id="modal-root" />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
