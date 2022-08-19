2122991e7dfc12d90c6f0c52e7592bda1caedfd7
김동현G <donghyeon@toss.im>
2022-08-19 19:43:57 +0900

    feat: 캐러셀 컴포넌트 적용

--- src/pages/MyWard/index.tsx
+++ src/pages/MyWard/index.tsx
@@ -1,9 +1,11 @@
 /* eslint-disable react/no-unused-prop-types */
 import { useQuery } from 'react-query';
+import styled from 'styled-components';
 import Flex from '@/src/components/Flex';
 import WardInfoContainer from './components/WardInfoContainer';
 import { getMyWard } from '@/pages/my-ward';
 import { Footer } from '../OpenInquiry/components/styledComponent';
+import Carousel from '@/src/components/Carousel';
 
 interface Ward {
   id: string;
@@ -21,17 +23,23 @@ function MyWard() {
   return (
     <Flex style={{ padding: 16, position: 'relative', width: '100vw', height: '100vh' }}>
       <Footer>
-        <WardInfoContainer type="new" location="강남구" remainDays="D-10" />
-        {wardList &&
-          wardList.map(({ id, name, remainDays }: Ward) => (
-            <WardInfoContainer key={id} type="existing" location={name} remainDays={remainDays} />
-          ))}
+        <StyledCarousel>
+          <WardInfoContainer type="new" location="강남구" remainDays="D-10" />
+          {wardList &&
+            wardList.map(({ id, name, remainDays }: Ward) => (
+              <WardInfoContainer key={id} type="existing" location={name} remainDays={remainDays} />
+            ))}
+        </StyledCarousel>
       </Footer>
     </Flex>
   );
 }
 
 export default MyWard;
+
+export const StyledCarousel = styled(Carousel)`
+  width: calc(100% - 16px);
+`;
 
 // <button onClick={() => setIsOpen(true)}>모달 테스트를 위한 버튼</button>
 // {isOpen && (
