2991497c1c460234c59558500a171112b49d67de
DongHyeon Kim <49136186+Kimbangg@users.noreply.github.com>
2022-08-25 01:13:52 +0900

    feat(open-inquiry): 질문 공유하기 페이지 API 연동 (#47)
    * chore: install next-swc pacakage
    
    * feat: 성향 및 질문 리스트에 Padding 추가
    
    * feat: 질문 공유하기 페이지 API 연동
    
    Co-authored-by: 김동현G <donghyeon@toss.im>

--- src/pages/OpenInquiry/index.tsx
+++ src/pages/OpenInquiry/index.tsx
@@ -1,9 +1,10 @@
+/* eslint-disable react/no-unused-prop-types */
 import Image from 'next/image';
 
+import { useQuery } from 'react-query';
 import thumbNail from '@/src/asset/image/thumbNail.png';
 import Flex from '@/src/components/Flex';
 import Tag from '@/src/components/Tag';
-import getUserInfo from './remote/getUserInfo';
 import {
   Description,
   Footer,
@@ -17,50 +18,54 @@ import {
 import QuestionContainer from './components/QuestionContainer';
 import TendencyContainer from './components/TendencyContainer';
 import TwoLayerContainer from './components/TwoLayerComponent';
+import { getAnsweredQuestion, getMyInfo } from '@/pages/open-inquiry';
 
 function OpenInquiry() {
-  const { nickname, level, profileDescription, representativeWardName, tags, questions } =
-    getUserInfo();
+  const { data: userData } = useQuery(['openInquiry/getMyInfo'], getMyInfo);
+  const { data: questionData } = useQuery(['openInquiry/getAnsweredQuestion'], getAnsweredQuestion);
 
   return (
     <>
-      <Header>
-        <span>{nickname}님은,</span>
-        <br />
-        <span style={{ fontWeight: 700 }}>
-          <StyledSpan>{representativeWardName}</StyledSpan>에서 도리도리 중!
-        </span>
-      </Header>
+      {userData && (
+        <>
+          <Header>
+            <span>{userData.nickname}님은,</span>
+            <br />
+            <span style={{ fontWeight: 700 }}>
+              <StyledSpan>어디선가</StyledSpan>에서 도리도리 중!
+            </span>
+          </Header>
 
-      <Section>
-        <Image src={thumbNail} alt="유저 이미지" width={250} height={250} />
-        <Flex direction="column" align="center" style={{ marginTop: 20, width: '200px' }}>
-          <TwoLayerContainer
-            top={
-              <Flex justify="center" align="center">
-                <Title>{nickname}</Title>
-                <Tag style={{ marginLeft: 8 }}>Lv.{level}</Tag>
-              </Flex>
-            }
-            bottom={<Description>{profileDescription}</Description>}
-          />
-        </Flex>
-      </Section>
+          <Section>
+            <Image src={thumbNail} alt="유저 이미지" width={250} height={250} />
+            <Flex direction="column" align="center" style={{ marginTop: 20, width: '200px' }}>
+              <TwoLayerContainer
+                top={
+                  <Flex justify="center" align="center">
+                    <Title>{userData.nickname}</Title>
+                    <Tag style={{ marginLeft: 8 }}>Lv.{userData.level}</Tag>
+                  </Flex>
+                }
+                bottom={<Description>{userData.profileDescription}</Description>}
+              />
+            </Flex>
+          </Section>
+        </>
+      )}
 
-      <Footer>
-        <QuestionButton onClick={() => {}}>질문하기</QuestionButton>
-        <StyledCarousel>
-          <TendencyContainer title="성향" tags={tags} />
-          {questions.map(({ questionId, questionContent, answerContent }) => (
-            <QuestionContainer
-              key={questionId}
-              id={questionId}
-              title={questionContent}
-              content={answerContent}
-            />
-          ))}
-        </StyledCarousel>
-      </Footer>
+      {questionData && (
+        <Footer>
+          <QuestionButton onClick={() => {}}>질문하기</QuestionButton>
+          <StyledCarousel>
+            <TendencyContainer title="성향" tags={userData.tags} />
+            {questionData.questions.map(
+              ({ id, content, answer }: { id: string; content: string; answer: any }) => (
+                <QuestionContainer key={id} id={id} title={content} content={answer.content} />
+              ),
+            )}
+          </StyledCarousel>
+        </Footer>
+      )}
     </>
   );
 }
