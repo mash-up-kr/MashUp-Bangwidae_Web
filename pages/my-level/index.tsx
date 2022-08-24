93736bdb30397612dd1dd7cf92d2103f7eccd5e9
yeomhyeseon <hyeseon.yeom@meshkorea.net>
2022-08-24 23:22:33 +0900

    feat: getServerSideProps 임시 주석 처리

--- pages/my-level/index.tsx
+++ pages/my-level/index.tsx
@@ -1,20 +1,20 @@
-import { dehydrate, QueryClient } from 'react-query';
+// import { dehydrate, QueryClient } from 'react-query';
 import api from 'src/api/core';
-import { LEVEL_POLICY, MY_LEVEL } from '@/src/consts/query';
+// import { LEVEL_POLICY, MY_LEVEL } from '@/src/consts/query';
 
 export { default } from 'pages/MyLevel';
 
-export async function getServerSideProps() {
-  const queryClient = new QueryClient();
-  await queryClient.prefetchQuery([MY_LEVEL], getMyLevel);
-  await queryClient.prefetchQuery([LEVEL_POLICY], getLevelPolicy(0));
+// export async function getServerSideProps() {
+//   const queryClient = new QueryClient();
+//   await queryClient.prefetchQuery([MY_LEVEL], getMyLevel);
+//   await queryClient.prefetchQuery([LEVEL_POLICY], getLevelPolicy(0));
 
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
-  };
-}
+//   return {
+//     props: {
+//       dehydratedState: dehydrate(queryClient),
+//     },
+//   };
+// }
 
 export function getMyLevel() {
   return api.get({
