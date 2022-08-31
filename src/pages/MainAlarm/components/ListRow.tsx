92926785b097aa30b2f8c75588f2e8e217264c59
김동현G <donghyeon@toss.im>
2022-09-01 01:43:25 +0900

    feat: 알림 페이지 API 적용

--- src/pages/MainAlarm/components/ListRow.tsx
+++ src/pages/MainAlarm/components/ListRow.tsx
@@ -1,18 +1,43 @@
 import { ReactNode } from 'react';
 import styled from 'styled-components';
+import Image from 'next/image';
 import Flex from '@/src/components/Flex';
-import TwoLayerContainer from '../../OpenInquiry/components/TwoLayerComponent';
+import answered from '@/src/asset/image/answered.png';
+import recevied from '@/src/asset/image/recevied.png';
+import levelup from '@/src/asset/image/levelup.png';
+import expired from '@/src/asset/image/expired.png';
 
 interface Props {
   title: ReactNode;
+  type: 'QUESTION_RECEIVED' | 'QUESTION_ANSWERED' | 'LEVEL_UP';
   content: ReactNode;
+  createdAt: string;
 }
 
-function AlarmListRow({ title, content }: Props) {
+const IMAGE_TYPE = {
+  QUESTION_RECEIVED: answered,
+  QUESTION_ANSWERED: recevied,
+  LEVEL_UP: levelup,
+  EXPIRED: expired,
+};
+
+function AlarmListRow({ type, title, content, createdAt }: Props) {
+  const [month, day] = createdAt.split('.');
+
   return (
-    <Flex direction="row">
-      <ImageWrapper />
-      <TwoLayerContainer top={<AlarmTopWrapper>{title}</AlarmTopWrapper>} bottom={content} />
+    <Flex direction="row" style={{ width: '100%', padding: '8px 16px', margin: '15px 0' }}>
+      <ImageWrapper>
+        <Image src={IMAGE_TYPE[type]} width={22} height={22} />
+      </ImageWrapper>
+      <Flex direction="column" style={{ width: '100%' }}>
+        <Flex direction="row" justify="space-between" style={{ width: '100%' }}>
+          <QuestionWrapper>{title}</QuestionWrapper>
+          <CreatedDaysWrapper>
+            {month}월 {day}일
+          </CreatedDaysWrapper>
+        </Flex>
+        <div style={{ maxWidth: 200, fontSize: 17 }}>{content}</div>
+      </Flex>
     </Flex>
   );
 }
@@ -20,20 +45,30 @@ function AlarmListRow({ title, content }: Props) {
 export default AlarmListRow;
 
 const ImageWrapper = styled.div`
-  width: 42px;
-  height: 42px;
-  border-radius: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 55px;
+  height: 45px;
+
+  margin: 14px 12px 0 0;
+  background: ${({ theme }) => theme.color.gray.Gray900};
+  border: 1px solid #252525;
+  border-radius: 50%;
 
   & > image {
     object-fit: cover;
   }
 `;
 
-const AlarmTopWrapper = styled.div`
-  margin-bottom: 8px;
-  overflow: hidden;
+const QuestionWrapper = styled.div`
+  max-width: 145;
+  margin-bottom: 12px;
+  color: ${({ theme }) => theme.color.gray.Gray200};
+  font-size: 12px;
+`;
+
+const CreatedDaysWrapper = styled.div`
   color: ${({ theme }) => theme.color.gray.Gray500};
   font-size: 12px;
-  white-space: nowrap;
-  text-overflow: ellipsis;
 `;
--- src/pages/MainAlarm/index.tsx
+++ src/pages/MainAlarm/index.tsx
@@ -1,16 +1,31 @@
 import { useQuery } from 'react-query';
+import { dateTime } from 'src/utils/DateTime';
 import AlarmListRow from './components/ListRow';
 import fetchAlarmList, { ALARM_QUERY_KEY } from './remote/fetchAlarmList';
+
+interface Alarm {
+  id: string;
+  type: 'QUESTION_RECEIVED' | 'QUESTION_ANSWERED' | 'LEVEL_UP';
+  title: string;
+  content: string;
+  read: boolean;
+  urlScheme: string;
+  createdAt: Date;
+}
 
 function MainAlarm() {
   const { data, isLoading } = useQuery(ALARM_QUERY_KEY.ALRAM, fetchAlarmList);
 
   if (isLoading) return null;
 
-  console.log(data.values);
-
-  return data.values.map((alarm: any) => (
-    <AlarmListRow title={alarm.title} content={alarm.content} createdAt={} />
+  return data.values.map((alarm: Alarm) => (
+    <AlarmListRow
+      key={alarm.id}
+      type={alarm.type}
+      title={alarm.title}
+      content={alarm.content}
+      createdAt={dateTime.format(alarm.createdAt, 'MM.DD')}
+    />
   ));
 }
 
