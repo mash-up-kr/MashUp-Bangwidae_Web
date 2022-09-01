63a23384785630917f6dbd50da3e9a2c1747a5fd
DongHyeon Kim <49136186+Kimbangg@users.noreply.github.com>
2022-09-02 00:57:16 +0900

    feat: Early Return 적용 (#64)
    Co-authored-by: 김동현G <donghyeon@toss.im>

--- src/pages/OpenInquiry/index.tsx
+++ src/pages/OpenInquiry/index.tsx
@@ -31,48 +31,44 @@ function OpenInquiry() {
     });
   };
 
+  if (userData == null || questionData == null) return null;
+
   return (
     <>
-      {userData && (
-        <>
-          <Header>
-            <span>{userData.nickname}님은,</span>
-            <br />
-            <span style={{ fontWeight: 700 }}>
-              <StyledSpan>어디선가</StyledSpan>에서 도리도리 중!
-            </span>
-          </Header>
+      <Header>
+        <span>{userData.nickname}님은,</span>
+        <br />
+        <span style={{ fontWeight: 700 }}>
+          <StyledSpan>어디선가</StyledSpan>에서 도리도리 중!
+        </span>
+      </Header>
 
-          <Section>
-            <Image src={thumbNail} alt="유저 이미지" width={250} height={250} />
-            <Flex direction="column" align="center" style={{ marginTop: 20, width: '200px' }}>
-              <TwoLayerContainer
-                top={
-                  <Flex justify="center" align="center">
-                    <Title>{userData.nickname}</Title>
-                    <Tag style={{ marginLeft: 8 }}>Lv.{userData.level}</Tag>
-                  </Flex>
-                }
-                bottom={<Description>{userData.profileDescription}</Description>}
-              />
-            </Flex>
-          </Section>
-        </>
-      )}
+      <Section>
+        <Image src={thumbNail} alt="유저 이미지" width={250} height={250} />
+        <Flex direction="column" align="center" style={{ marginTop: 20, width: '200px' }}>
+          <TwoLayerContainer
+            top={
+              <Flex justify="center" align="center">
+                <Title>{userData.nickname}</Title>
+                <Tag style={{ marginLeft: 8 }}>Lv.{userData.level}</Tag>
+              </Flex>
+            }
+            bottom={<Description>{userData.profileDescription}</Description>}
+          />
+        </Flex>
+      </Section>
 
-      {questionData && (
-        <Footer>
-          <QuestionButton onClick={() => handleDeepLinkClick('question')}>질문하기</QuestionButton>
-          <StyledCarousel>
-            <TendencyContainer title="성향" tags={userData.tags} />
-            {questionData.questions.map(
-              ({ id, content, answer }: { id: string; content: string; answer: any }) => (
-                <QuestionContainer key={id} id={id} title={content} content={answer.content} />
-              ),
-            )}
-          </StyledCarousel>
-        </Footer>
-      )}
+      <Footer>
+        <QuestionButton onClick={() => handleDeepLinkClick('question')}>질문하기</QuestionButton>
+        <StyledCarousel>
+          <TendencyContainer title="성향" tags={userData.tags} />
+          {questionData.questions.map(
+            ({ id, content, answer }: { id: string; content: string; answer: any }) => (
+              <QuestionContainer key={id} id={id} title={content} content={answer.content} />
+            ),
+          )}
+        </StyledCarousel>
+      </Footer>
     </>
   );
 }
