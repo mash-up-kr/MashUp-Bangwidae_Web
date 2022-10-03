5de36fc09c6ca52ea77e1c1327851e51a412a061
김동현G <donghyeon@toss.im>
2022-10-03 18:54:35 +0900

    feat: next ssr 빌드 시, styled-components가 적용되도록 document 업데이트

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,38 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+  DocumentInitialProps,
+} from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originRenderPage({
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
   render() {
     return (
       <Html>
